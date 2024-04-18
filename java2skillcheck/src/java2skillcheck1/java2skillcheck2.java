e7434526297c5a757b93ed661e8639ab3d94c5e0
matsumuraryouga <matsumuraryouga@192.168.10.101>
2024-04-18 23:02:43 +0900

    java2skillcheck

--- java2skillcheck/src/java2skillcheck1/java2skillcheck2.java
+++ java2skillcheck/src/java2skillcheck1/java2skillcheck2.java
@@ -1,6 +1,15 @@
 package java2skillcheck1;
 
+/**
+ * 2.演算子 実力確認問題
+ */
+
 public class java2skillcheck2 {
+	
+	/**
+	 * 変数1に”Hello”を代入し、変数2に”World”を代入し、2つの文字列を結合させて”Hello World”を表示
+	 * @param args 出力される文字列
+	 */
 	
 	public static void main(String[] args) {
 		
--- java2skillcheck/src/java2skillcheck1/java2skillcheck4.java
+++ java2skillcheck/src/java2skillcheck1/java2skillcheck4.java
@@ -1,15 +1,28 @@
 package java2skillcheck1;
 
+/**
+ * 2.演算子　実力確認問題
+ */
+
 public class java2skillcheck4 {
+	
+	/**
+	 * int 型の変数 x に数値 7 を代入する。
+	 * 変数 x の値を 3 倍にする。
+	 * 変数 x の値を表示する。
+	 * 変数 x の値を半分(1/2)にする。
+	 * 変数 x の値を表示する。
+	 * @param args 整数
+	 */
 	
 	public static void main(String[] args) {
 		
 		int x = 7;
-		x = x * 3;
+		x *= 3;
 		
 		System.out.println(x);
 		
-		x = x / 2;
+		x /= 2;
 		
 		System.out.println(x);
 		
